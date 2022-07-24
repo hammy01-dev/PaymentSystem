d135b99d89e8956d3f36d622d191ed7c3be74c14
hammy01-dev <hamza.amer@devsinc.com>
2022-07-25 01:01:49 +0500

    heroku fix

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
-require "#{Rails.root}/app/services/transaction_creator"
+# require "#{Rails.root}/app/services/transaction_creator"
 class TransactionsController < ApplicationController
-  inculde Transactions
+  # inculde Transactions
   def abc
     # a = TransactionCreator.new
     data  = get_subscriptions
